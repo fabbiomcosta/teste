bf325132e5c66786eb5d4d55d9c97660299d280e
Fabio Costa <fabbiomcosta@gmail.com>
2015-10-27 17:48:19 -0300

    Servidor - cliente com basica comunicacao

--- client_chat.c
+++ client_chat.c
@@ -5,38 +5,115 @@
 
 */
 
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-//#include <sys/sockets.h>
-#include <netinet/in.h>
 #include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include  <signal.h>
 #include "messages.h"
 
-// Variables
-char CLIENT_NAME[30];
-int SERVER_ADDRESS;
-int SERVER_PORT;
+#define MAXDATASIZE 2000 // max number of bytes we can get at once 
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
 
 // Main Program
 int main (int argc, char *argv[])
 {
+
+    int sockfd, numbytes;  
+    char buf[MAXDATASIZE];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
     if (argc != 4)
     {
         printf ("\nUso: client_chat <CLIENT_NAME> <SERVER_ADDRESS> <SERVER_PORT>\n");
         return (1);
     }
-    else
-    {
-        strcpy(CLIENT_NAME, argv[1]);
-        SERVER_ADDRESS = atoi(argv[2]);
-        SERVER_PORT = atoi(argv[3]);
-        printf(MSG_CL_CONNECTED);
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(argv[2], argv[3], &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
     }
-    return (0);
 
-int mysocket;
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
 
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
 
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    printf(MSG_CL_CONNECTED);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (send(sockfd, argv[1], 30, 0) == -1) perror("send");
+
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+
+    buf[numbytes] = '\0';
+
+    printf("%s\n",buf);
+ 
+    void  INThandler(int sig) {
+         char  c;
+
+         signal(sig, SIG_IGN);
+         printf("\nDo you really want to quit? [y/n] ");
+         c = getchar();
+         if (c == 'y' || c == 'Y'){
+              if (send(sockfd, argv[1], 30, 0) == -1) perror("send");
+              exit(0);
+         }else
+              signal(SIGINT, INThandler);
+         getchar(); // Get new line character
+     }
+    
+    signal(SIGINT, INThandler);
+    while (1) pause();
+
+    close(sockfd);
+
+    return 0;
 }
+
+    
--- messages.h
+++ messages.h
@@ -9,7 +9,6 @@
 
 /********** SERVER MESSAGES **********/
 
-#define MSG_SRV_PORT "\nServer's listening on port %i ...\n"
 
 // <HOUR>:<MINUTES> \t <CLIENT_NAME> \t conectado.
 #define MSG_SRV_CLIENT_CONNECTED "%s \t %s \t Connected.\n"
@@ -18,13 +17,13 @@
 #define MSG_SRV_CLIENT_DESCONNECTED "%s \t %s \t Desconnected.\n"
 
 //<TIME> \t <CLIENT_NAME> \t <COMMAND> \t Executado:<Sim\Não>
-#define MSG_SRV_COMMAND "%s \t %s \t %s \t Executado: %s.\n"
+#define MSG_SRV_COMMAND "%s \t %s \t %s \t Executed: %s.\n"
 
 
 
 /********** CLIENT MESSAGES **************/
 
-#define MSG_CL_CONNECTED "Conectado com Sucesso\n"
+#define MSG_CL_CONNECTED "Successfully connected\n"
 
 // Falha se ja existe um cliente conectado
 #define MSG_CL_FAIL "Falha: %s em uso.\n"
